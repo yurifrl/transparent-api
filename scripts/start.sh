079039344197fdccd0f529858ba6e62afd149f22
yurifrl <yurifrl@hotmail.com>
2016-04-24 18:45:44 -0300

    api

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,5 +3,6 @@
 docker run --rm -ti \
   -e ENV=development \
   -w /app \
+  -p 8080:8080 \
   -v $(pwd):/app \
-  transparent-api scripts/inside-container.sh "$@"
+  transparent-api scripts/inside-container.sh npm start
--- test/index.js
+++ test/index.js
@@ -1,13 +1,15 @@
-import http from 'http';
-import assert from 'assert';
+import http from 'http'
+import assert from 'assert'
 
-import '../lib/index.js';
+import '../lib/index.js'
+
+const URL = 'http://127.0.0.1:8080/'
 
 describe('Example Node Server', () => {
   it('should return 200', done => {
-    http.get('http://127.0.0.1:1337', res => {
-      assert.equal(200, res.statusCode);
-      done();
+    http.get(URL, res => {
+      assert.equal(200, res.statusCode)
+      done()
     });
   });
 });
