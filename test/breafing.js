a498012df596f0db354d114810f0ae82e1ba3511
yurifrl <yurifrl@hotmail.com>
2016-04-25 09:45:42 -0300

    start breafing

---
+++ test/breafing.js
@@ -0,0 +1,10 @@
+import assert from 'assert'
+import Breafing from '../lib/breafing'
+
+describe('Breafing', () => {
+  it('should create breafing', done => {
+
+    breafing = new Breafing
+    breafing.name = "My nice breafing"
+  });
+});
